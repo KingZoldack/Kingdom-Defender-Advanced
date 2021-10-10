b65fce9828ada1aa549781c9a3a93c1191f518ff
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-10 16:02:59 +0100

    Nodes
    Added pure C# script with constructor.
    
    Created dictionary with for each loops to lay out grid.

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] Vector2Int _gridSize;
+    Dictionary<Vector2Int, Node> _grid = new Dictionary<Vector2Int, Node>();
+
+    private void Awake()
+    {
+        CreatGrid();
+    }
+
+    private void CreatGrid()
+    {
+        for (int x = 0; x < _gridSize.x; x++)
+        {
+            for (int y = 0; y < _gridSize.y; y++)
+            {
+                Vector2Int coordinates = new Vector2Int(x, y);
+                _grid.Add(coordinates, new Node(coordinates, true));
+                Debug.Log(_grid[coordinates].coordinates + " = " + _grid[coordinates].isWalkable);
+            }
+        }
+    }
+}
