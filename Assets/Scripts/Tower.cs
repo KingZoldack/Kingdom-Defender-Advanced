f01a973882ca2271458e0ce995ae854c8d26de2c
KingZoldack <50232232+KingZoldack@users.noreply.github.com>
2021-10-10 19:20:50 +0100

    Build Timer
    Added build timer to turrets. Spawns over time.
    
    Added particle effect to group trees.

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int _cost = 75;
+    [SerializeField] float _buildDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower _tower, Vector3 position)
     {
@@ -24,5 +30,27 @@ public class Tower : MonoBehaviour
 
         return false;
         
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(_buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
     }
 }
